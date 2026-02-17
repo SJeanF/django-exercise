9131b7fce3df98800e47d3369f69565e9a671b7b
Jean <fonsecasantosjean@gmail.com>
2026-02-17 11:26:45 -0300

    feat: adicionar e configurar view e urls de hello world

--- exerciseApp/library/views.py
+++ exerciseApp/library/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views import generic
+from django.http import HttpResponse
 # Create your views here.
+
+class HelloView(generic.View):
+  def get(self, request, *args, **kargs):
+    return HttpResponse('Hello World')
